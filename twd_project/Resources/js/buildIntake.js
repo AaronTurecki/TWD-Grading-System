9271017896c1e1d4f33bc48e67c4f6673cf3f9f1
Aaron Turecki <aaronturecki@gmail.com>
2014-04-16 18:58:35 -0700

    Grading App with ASP.Net MVC 4

---
+++ twd_project/Resources/js/buildIntake.js
@@ -0,0 +1,60 @@
+﻿//function addModule() {
+//    var displayType = '';
+//    displayType = document.getElementById("addModule").value;
+
+//    if (displayType == undefined || displayType == "none") {
+//        document.getElementById("addModule").style.display = "block";
+//        document.getElementById("addModule").value = "block";
+//        document.getElementById("removeModule").style.display = "none";
+//        document.getElementById("addStudent").style.display = "none";
+//        document.getElementById("removeStudent").style.display = "none";
+//    } else {
+//        document.getElementById("addModule").style.display = "none";
+//        document.getElementById("addModule").value = "none";
+//    }
+//}
+
+//function removeModule() {
+//    var displayType = document.getElementById('removeModule').value;
+
+//    if (displayType == undefined || displayType == "none") {
+//        document.getElementById("removeModule").style.display = "block";
+//        document.getElementById("removeModule").value = "block";
+//        document.getElementById("addModule").style.display = "none";
+//        document.getElementById("addStudent").style.display = "none";
+//        document.getElementById("removeStudent").style.display = "none";
+//    } else {
+//        document.getElementById("removeModule").style.display = "none";
+//        document.getElementById("removeModule").value = "none";
+//    }
+//}
+
+function addStudent() {
+    var displayType = document.getElementById('addStudent').value;
+
+    if (displayType == undefined || displayType == "none") {
+        document.getElementById("addStudent").style.display = "block";
+        document.getElementById("addStudent").value = "block";
+        document.getElementById("addModule").style.display = "none";
+        document.getElementById("removeModule").style.display = "none";
+        document.getElementById("removeStudent").style.display = "none";
+    } else {
+        document.getElementById("addStudent").style.display = "none";
+        document.getElementById("addStudent").value = "none";
+    }
+}
+
+function removeStudent() {
+    var displayType = document.getElementById('removeStudent').value;
+
+    if (displayType == undefined || displayType == "none") {
+        document.getElementById("removeStudent").style.display = "block";
+        document.getElementById("removeStudent").value = "block";
+        document.getElementById("addModule").style.display = "none";
+        document.getElementById("removeModule").style.display = "none";
+        document.getElementById("addStudent").style.display = "none";
+    } else {
+        document.getElementById("removeStudent").style.display = "none";
+        document.getElementById("removeStudent").value = "none";
+    }
+} 
---
+++ twd_project/ViewModels/CourseGradeModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using twd_project.Models;
+
+namespace twd_project.ViewModels {
+
+    public class CourseGradeModel {
+        public Intake             IntakeO      { get; private set; }
+        public List<Intake>       Intakes      { get; private set; }
+        public List<TWD_User>     Students     { get; private set; }
+        public List<List<string>> CourseGroup  { get; private set; }
+
+        public void SetIntakes(Intake intake, List<Intake> intakes) {
+            IntakeO = intake;
+            Intakes = intakes;
+        }
+
+        public void SetStudentHeadings(List<TWD_User> students) {
+            Students = students;
+        }
+
+        public void SetCourseGroup(List<List<string>> courseGroup) {
+            CourseGroup = courseGroup;
+        }
+    }
+}
